b20a90485d1e9e9911a9e6966380e8ac28ed9fbb
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-02-27 22:16:50 +0700

    menambahkan materi pengenalan Matlab

--- pengenalanMatlab.m
+++ pengenalanMatlab.m
@@ -1,1 +1,42 @@
-a = 10
+% PENGENALAN MATLAB
+
+clear % menghapus isi workspace
+clc % menghapus isi command Window
+
+% skalar
+a = 7;
+
+% Vector
+
+b = [ 1 2 3 4 5 ];
+c = [ 1;2;3;4;5 ];
+
+% Matrix
+
+d = [ 1 2;3 4 ];
+
+% transpose
+
+bt = b';
+ct = c';
+dt = d';
+
+% penjumlahan vector
+
+sum = b + c';
+
+% perkalian vector dot
+
+kaliDot = dot(b,ct);
+
+% perkalian vector cross
+
+e = [1 3 4];
+f = [6 7 8];
+kaliCross = cross(e, f);
+
+% penggabungan dua buah vector, concatenate
+
+g = [b e];
+h = [b; c'];
+i = [b;e 7 8];
